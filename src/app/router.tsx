aa4df2ff993beec6738a27db82f48e9f593bd303
Dmitri <d.perminov@fact.digital>
2025-07-08 21:03:54 +0500

    ft: add axios and redux + auth

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,11 +1,26 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MainLayout from '../pages/main-layout/MainLayout.tsx';
 import WorkSpacesPage from '../pages/work-spaces/WorkSpacesPage.tsx';
+import axios from 'axios';
+
+const login = (): void => {
+    axios
+        .post('http://localhost:8080/api/auth/login', {
+            username: 'admian',
+            password: 'adminpassword',
+        })
+        .then((response) => {
+            console.log(response.data.token);
+            localStorage.setItem('token', response.data.token);
+            console.log('logged!');
+        });
+};
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <MainLayout />,
+        loader: login,
         children: [
             {
                 index: true,
