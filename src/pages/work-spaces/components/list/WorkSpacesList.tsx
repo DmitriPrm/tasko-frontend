26a940f519dd45801ab598706ae3cf856285e71f
Dmitri <d.perminov@fact.digital>
2025-07-08 23:45:11 +0500

    ft: add api for work spaces

--- src/pages/work-spaces/components/list/WorkSpacesList.tsx
+++ src/pages/work-spaces/components/list/WorkSpacesList.tsx
@@ -1,18 +1,25 @@
 import WorkSpacesListItem from './WorkSpacesListItem.tsx';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Skeleton } from '@mui/material';
 
 const WorkSpacesList = () => {
-    const workSpaces = [
-        {
-            id: 1,
-        },
-        {
-            id: 2,
-        },
-    ];
-    return (
+    const [workSpaces, setWorkSpaces] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+        axios
+            .get('http://localhost:8080/api/work-space')
+            .then((response) => setWorkSpaces(response.data))
+            .finally(() => setIsLoading(false));
+    }, []);
+    return isLoading ? (
+        <Skeleton />
+    ) : (
         <div className="grid grid-cols-4 gap-4">
             {workSpaces.map((workSpace) => (
-                <WorkSpacesListItem key={workSpace.id} />
+                <WorkSpacesListItem key={workSpace.id} workSpace={workSpace} />
             ))}
         </div>
     );
--- src/pages/work-spaces/components/list/WorkSpacesListItem.tsx
+++ src/pages/work-spaces/components/list/WorkSpacesListItem.tsx
@@ -1,16 +1,15 @@
-import { Button } from '@mui/material';
+import { Button, Card } from '@mui/material';
 
-const WorkSpacesListItem = () => {
+const WorkSpacesListItem = ({ workSpace }) => {
     return (
-        <div className="border-1 border-gray-400 rounded-2xl">
+        <Card>
             <img
-                className="rounded-t-2xl"
                 src={
                     'https://avatars.mds.yandex.net/i?id=353a756d104d8b145578defe460c9d7b_l-5333398-images-thumbs&n=13'
                 }
             />
             <div className="flex flex-col gap-2 p-4">
-                <h4 className="font-bold text-xl">Title</h4>
+                <h4 className="font-bold text-xl">{workSpace.title}</h4>
                 <p className="text-gray-600">Description for the card</p>
                 <ul>
                     <li className="flex justify-between">
@@ -24,7 +23,7 @@ const WorkSpacesListItem = () => {
                 </ul>
                 <Button variant="contained">Enter</Button>
             </div>
-        </div>
+        </Card>
     );
 };
 
