41f02ab0c0a7e66be03574dc4d3eba3a6059d048
Dmitri <d.perminov@fact.digital>
2025-07-10 22:00:41 +0500

    replace fun

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,27 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MainLayout from '../pages/main-layout/MainLayout.tsx';
 import WorkSpacesPage from '../pages/work-spaces/WorkSpacesPage.tsx';
-import axios from 'axios';
-import { login, setToken } from '../store/slices/authSlice.ts';
-import { store } from '../store/store.ts';
-import { setTokenInApp } from '../providers/auth/auth.ts';
-
-const loginToSystem = (): void => {
-    const token = localStorage.getItem('token');
-
-    if (token) {
-        store.dispatch(setToken(token));
-        store.dispatch(login());
-        return;
-    }
-
-    axios
-        .post('http://localhost:8080/api/auth/login', {
-            username: 'admian',
-            password: 'adminpasswosrd',
-        })
-        .then((response) => setTokenInApp(response.data.token));
-};
+import { loginToSystem } from '../providers/auth/auth.ts';
 
 const router = createBrowserRouter([
     {
--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,5 +1,6 @@
 import { store } from '../../store/store.ts';
-import { login, logout } from '../../store/slices/authSlice.ts';
+import { login, logout, setToken } from '../../store/slices/authSlice.ts';
+import axios from 'axios';
 
 export const destroyTokenInApp = () => {
     localStorage.removeItem('token');
@@ -9,4 +10,21 @@ export const destroyTokenInApp = () => {
 export const setTokenInApp = (token: string) => {
     localStorage.setItem('token', token);
     store.dispatch(login());
+};
+
+export const loginToSystem = (): void => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+        store.dispatch(setToken(token));
+        store.dispatch(login());
+        return;
+    }
+
+    axios
+        .post('http://localhost:8080/api/auth/login', {
+            username: 'admian',
+            password: 'adminpasswosrd',
+        })
+        .then((response) => setTokenInApp(response.data.token));
 };
