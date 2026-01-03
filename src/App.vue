9b3a7bd0ac6c8269957d5fe1d8ef02e12c398fe6
DmitriPrm <perminov.dima1@mail.ru>
2026-01-03 22:14:44 +0500

    add element plus

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
+
+import { ref } from 'vue'
+
+const v = ref();
+
 </script>
 
 <template>
-  <RouterView />
+  <div class="bg-gray-200">
+    <el-card>
+      haha
+    </el-card>
+  </div>
+    <RouterView />
 </template>
 
 <style scoped lang="scss">
